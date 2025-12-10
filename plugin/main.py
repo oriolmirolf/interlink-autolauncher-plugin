b0bed3b1aae34e50f0023947f1e205f869926adc
Oriol Miró <111059811+oriolmirolf@users.noreply.github.com>
2025-12-10 11:09:06 +0100

    Add Response import to main.py

--- plugin/main.py
+++ plugin/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 import json
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 import uvicorn
 import interlink
 from plugin.provider_autolauncher import AutolauncherProvider
